5053146be1bc4ed5cebd46e9417032cceba67907
VivekYadavInductus <inductusvivek@gmail.com>
2023-10-16 18:51:12 +0530

    Pointer Done

---
+++ pointer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    /*What Is Pointer?
+    Pointer in a datatype which hold the address of Other datatypes;
+
+    &----->Find(Get) Address of Operator
+    *-----> (Store The Data in RAM Of System)Derefernces of Operator.
+    */
+
+    int a = 3;
+
+    int *b = &a;
+    /* &-----> Address of Operator*/
+    cout << "The Address Of a is: " << &a << endl;
+    cout << "The Address Of a is: " << b << endl;
+
+    /* *(Value at)-----> Derefernces of Operator*/
+    cout << "The Value Of Address of b is: " << *b << endl;
+
+    return 0;
+}
